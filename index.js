ad1edc109671e6606398e3ad84a3a3a0cdfa323d
Aya Ali <1aya.dn.ali@gmail.com>
2022-08-08 14:47:23 +0300

    finished Task

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let  employee = {
+    name: "Aya",
+    address:"iraq",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key , value){
+    let newObj ={...employee};
+    newObj[key] = value;
+    return newObj ;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key , value){
+    employee[key] = value;
+    return employee;
+
+}
+// console.log(employee);
+ function deleteFromEmployeeByKey(employee,name){
+     let  newDeleteObj = {...employee};
+     delete  newDeleteObj.name;
+      console.log(newDeleteObj );
+
+    return  newDeleteObj ;
+ }
+ function destructivelyDeleteFromEmployeeByKey(){
+    delete  employee.name;
+    return employee ;
+
+ }
